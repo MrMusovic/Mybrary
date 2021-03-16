fc1e686f1b7769364f78c9dc9e6e740bf9197fef
Dervish Musovic <dervish.musovic@education.wa.edu.au>
2021-03-16 15:12:00 +0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,28 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views') //to fix
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose') //import mongoose form library
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewURLParser: true})
+//when app deployed you want to connect to server on web
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', error => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000) //pulls from environment variable
